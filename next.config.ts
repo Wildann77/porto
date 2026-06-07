4833e7857657be5d23a2f3816d1cadae71bce38f
Wildann77 <boyblanco77@gmail.com>
2026-06-07 21:17:06 +0700

    chore: update build and linter configurations

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    optimizePackageImports: ["lucide-react", "framer-motion"],
+  },
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
 };
 
 export default nextConfig;
